88ce8dd975005f807c29f2bc1c21f2efe6c751f5
Aditya Gavandi <140941243+adityagavandi61@users.noreply.github.com>
2024-01-13 20:57:30 +0530

    first commit

---
+++ socialmedia/views.py
@@ -0,0 +1,20 @@
+#created by me
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'index.html')
+
+def login(request):
+    return render(request,'login.html')
+    
+def newac(request):
+    return render(request,'newac.html')
+
+def userregister(request):
+    return render(request,'userregister.html')
+
+def viewerregister(request):
+    return render(request,'viewerregister.html')
---
+++ socialweb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SocialwebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'socialweb'
